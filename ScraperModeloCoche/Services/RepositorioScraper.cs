8b33e44fa64353cd96cfb9aefd22cbc61fb05a36
jfmarin@orvipal.com <jfmarin@orvipal.com>
2025-03-17 15:34:44 +0100

    Se añadió scrapeo para pagina web de especificaciones + en trabajo la de los modelos

--- ScraperModeloCoche/Services/RepositorioScraper.cs
+++ ScraperModeloCoche/Services/RepositorioScraper.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Threading.Tasks;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
